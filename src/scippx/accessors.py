4e8be0db3a177b45a437f660728f9c7980ef185f
Simon Heybrock <simon.heybrock@ess.eu>
2022-09-14 09:29:02 +0200

    Add accessor with quantity

--- src/scippx/accessors.py
+++ src/scippx/accessors.py
@@ -1,7 +1,9 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
 # @author Simon Heybrock
+from typing import Dict
 import xarray as xr
+import pint
 
 
 @xr.register_dataarray_accessor('masks')
@@ -12,3 +14,16 @@ class MaskAccessor:
 
     def __getitem__(self, name: str):
         return xr.Variable(dims=self._obj.dims, data=self._obj.data.masks[name])
+
+
+@xr.register_dataarray_accessor('qsel')
+class IndexAccessor:
+
+    def __init__(self, xarray_obj):
+        self._obj = xarray_obj
+
+    def __getitem__(self, sel: Dict[str, pint.Quantity]):
+        for dim, q in sel.items():
+            if self._obj.coords[dim].data.units != q.units:
+                raise KeyError("Key has wrong unit.")
+        return self._obj.loc[{dim: q.magnitude for dim, q in sel.items()}]
